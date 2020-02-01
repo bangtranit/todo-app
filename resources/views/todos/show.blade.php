c030153b8304fca90590adffe84fc0e02f8d5d58
thanhbang <thanhbang.it.dn2000@gmail.com>
2020-02-02 00:11:32 +0900

    add layout

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,28 +1,19 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-          integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <title>Document</title>
-</head>
-<body>
-    <div class="container">
-        <h1 class="text-center">{{$todo->name}}</h1>
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card card-default">
-                    <div class="card-header">
+@extends('layout.app')
+@section('title')
+    Single todo: {{$todo->name}}
+@endsection
+@section('content')
+    <h1 class="text-center">{{$todo->name}}</h1>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card card-default">
+                <div class="card-header">
                         Todos
                     </div>
-                    <div class="card-body">
-                        {{$todo->description}}
-                    </div>
+                <div class="card-body">
+                    {{$todo->description}}
                 </div>
             </div>
+        </div>
     </div>
-</body>
-</html>
+@endsection
